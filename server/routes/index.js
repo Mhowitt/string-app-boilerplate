e2c9d7554c37b10fccb09ce81554a750556d1114
Miranda Howitt <miranda.howitt@gmail.com>
2018-11-01 21:31:25 -0700

    server up and running react-boilerplate cleaned and homepage and header running with one styledcomponent

---
+++ server/routes/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
+
+router
+  .route('')
+  .get(async (req, res, next) => {
+    try {
+      const strings = await db.query('SELECT * FROM strings');
+      if (strings.rows.length < 1) {
+        return res.json('No strings :(');
+      } else {
+        return res.json(strings.rows);
+      }
+    } catch (err) {
+      return next(err);
+    }
+  })
+  .post(async (req, res, next) => {
+    try {
+      const newStrings = await db.query(
+        'INSERT INTO strings (string) VALUES ($1) RETURNING *',
+        [req.body.string]
+      );
+      //returning all
+      return res.json(newStrings.rows[0]);
+    } catch (err) {
+      return next(err);
+    }
+  });
+
+module.exports = router;
